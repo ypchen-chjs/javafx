112dbc1a0cf1eb6b1117d6f1eedd064b56e89d5f
yiping <jc5230@gmail.com>
2013-08-14 20:14:47 +0800

    0817

--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -76,6 +76,9 @@ public class Login extends Application {
         
         Scene scene = new Scene(grid,300,275);
         primaryStage.setScene(scene);
+        scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
+        
+        
         primaryStage.show();
     }
 
